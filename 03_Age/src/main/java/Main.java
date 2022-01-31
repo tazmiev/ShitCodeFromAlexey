8cf31a4aeb38cc2381341ed8f95c0f65d50f0cc5
tazmiev <tazmiev@gmail.com>
2022-01-31 23:24:24 +0300

    Task 3 is added;

---
+++ 03_Age/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int vasyaAge = 29;
+        int katyaAge = 14;
+        int mishaAge = 36;
+
+        int min = -1;
+        int middle = -1;
+        int max = -1;
+    }
+}
