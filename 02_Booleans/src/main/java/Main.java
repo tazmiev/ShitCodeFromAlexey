905092e62e6d86fd87da3ba43bd1404ab599dfd3
Delian <mehatronika01@gmail.com>
2022-03-29 15:53:45 +0400

    add solutions of Task 2 and Task 3

--- 02_Booleans/src/main/java/Main.java
+++ 02_Booleans/src/main/java/Main.java
@@ -14,12 +14,15 @@ public class Main {
         }
 
         //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-        System.out.println("Pancakes");
+        if (powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >= 30)
+            System.out.println("Pancakes");
 
         //milk - 300 ml, powder - 5 g, eggs - 5
-        System.out.println("Omelette");
+        if (milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 3)
+            System.out.println("Omelette");
 
         //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-        System.out.println("Apple pie");
+        if (appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4)
+            System.out.println("Apple pie");
     }
 }
